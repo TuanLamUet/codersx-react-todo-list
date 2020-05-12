70994f94ff6f531897ff950afd796efeba5465d1
TuanLamUet <tupikano1@gmail.com>
2020-05-12 17:21:25 +0700

    firts commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Todos() {
+  const todo = [
+    'Go to market',
+    'Buy food',
+    'Make dinner'
+  ];
+  return(
+    <ul>
+      {todo.map((item) => <li>{item}</li>)}
+    </ul>
+  )
+}
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +31,7 @@ function App() {
         >
           Learn React
         </a>
+        <Todos/>
       </header>
     </div>
   );
